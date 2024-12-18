57ddf8c06a80f28bf9d73cc5abcff693f820c4ac
kundannsharmaa <sha.kundan@concret.io>
2024-12-18 17:43:50 +0530

    ci: [validate]updated test case

--- test/commands/template/validate.nut.ts
+++ test/commands/template/validate.nut.ts
@@ -6,15 +6,7 @@ describe('TemplateValidate Command', () => {
   const testDir = path.join(process.cwd(), 'data_gen/templates');
   const testFile = path.join(testDir, 'testTemplateValidate.json');
 
-//   const verifyCommandOutput = (stdout: string, expectedMessage: string, done: Mocha.Done) => {
-//     // console.log('Command Output:', stdout);
-//     if (!stdout.includes(expectedMessage)) {
-//       return done(new Error(`Expected message "${expectedMessage}" not found in stdout.`));
-//     }
-//     done();
-//   };
-
-//   const templateName = 'testTemplatevalidate';
+  const templateName = 'testTemplatevalidate';
   const invalidTemplateName = 'testTemplateInvalid';
   before(() => {
     if (!fs.existsSync(testDir)) fs.mkdirSync(testDir, { recursive: true });
@@ -33,12 +25,12 @@ describe('TemplateValidate Command', () => {
           },
         },
         {
-            account: {
-              language: 'en',
-              count: 25,
-              fieldsToExclude: ['cleanstatus', 'jigsaw'],
-            },
+          account: {
+            language: 'en',
+            count: 25,
+            fieldsToExclude: ['cleanstatus', 'jigsaw'],
           },
+        },
       ],
     };
     fs.writeFileSync(testFile, JSON.stringify(sampleData, null, 2), 'utf8');
@@ -50,34 +42,25 @@ describe('TemplateValidate Command', () => {
     }
   });
 
-
-//   it('[BS_TC_146] Validate the behavior of the validate command with the correct template name', (done) => {
-//     const command = `sf template validate -t ${templateName}`;
-//     exec(command, (error, stdout) => {
-//         if (error) {
-//             return done(error);
-//         }
-//         console.log(stdout);
-        
-//         const commonMsg = 'Success: SF Connection established.';
-//         const expectedValidationMsg = "Successfully validated 'testTemplatevalidate.json' and no invalid object/fields were found!";
-//         if (!stdout.includes(commonMsg) || !stdout.includes(expectedValidationMsg)) {
-//             return done(new Error('some of the Expected messages not found in stdout.'));
-//         }
-//         done();
-//     });
-//   });
-
+  it('[BS_TC_146] Validate the behavior of the validate command with the correct template name', (done) => {
+    const command = `sf template validate -t ${templateName}`;
+    exec(command, (error, stdout) => {
+        if (error) {
+            return done(error);
+        }
+        const commonMsg = 'Success: SF Connection established.';
+        const expectedValidationMsg = "Successfully validated 'testTemplatevalidate.json' and no invalid object/fields were found!";
+        if (!stdout.includes(commonMsg) || !stdout.includes(expectedValidationMsg)) {
+            return done(new Error('some of the Expected messages not found in stdout.'));
+        }
+        done();
+    });
+  });
 
   it('[BS_TC_147] Validate the behavior of the validate command with an incorrect template name', (done) => {
     const command = `sf template validate -t ${invalidTemplateName}` ;
     exec(command, (error, stdout, stderr) => {
-        console.log(stdout);
-        console.log(stderr);
-
-        
-
-        const expectedErrorMessage = 'Error (1): File: testTemplateInvalid is not present at this path: C:\Users\Kundan Kumar Sharma\Documents\SMOCKER\data_gen\templates\testTemplateInvalid.json'
+        const expectedErrorMessage = `File: testTemplateInvalid is not present at this path: ${path.join('C:', 'Users', 'Kundan Kumar Sharma', 'Documents', 'SMOCKER', 'data_gen', 'templates', 'testTemplateInvalid.json')}`;
         if (stderr.includes(expectedErrorMessage)) {
         done();
       } else {
@@ -86,36 +69,44 @@ describe('TemplateValidate Command', () => {
     });
   });
 
-//   it('[BS_TC_148] Validate the behavior when an invalid object is present in the template.', (done) => {
-//     const command = `sf template upsert -t ${templateName}`;
-//     exec(command, (error, stdout,stderr) => {
-//         if (error) {
-//             return done(error);
-//         }
-//         console.log(stdout);
-//         console.log(stderr);
+  /* Note: Change the mock data according to the scenario for invalid object and its fields */
 
-//         const commonMsg = 'Success: SF Connection established.';
-//         const expectedValidationMsg = "Successfully validated 'testTemplatevalidate.json' and no invalid object/fields were found!";
-//         if (!stdout.includes(commonMsg) || !stdout.includes(expectedValidationMsg)) {
-//             return done(new Error('some of the Expected messages not found in stdout.'));
-//         }
-//         done();
-//     });
-//   });
+  it('[BS_TC_148] Validate the behavior when an invalid object is present in the template.', (done) => {
+    const command = `sf template validate -t ${templateName}`;
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error('Command Execution Error:', error);
+        return done(error);
+      }
+      const successMessage = 'Success: SF Connection established.';
+      const warningMessage = 'Warning: SObjects do not exist or cannot be accessed:\n -> contacts';
+      const noteMessage =
+        'Note: Still we keep these populated these values, You can change them anytime from the data template!';
 
-// it('[BS_TC_149] Validate the behavior when an invalid field is used', (done) => {
-//     const command = `sf template upsert -t ${templateName} -w en -c 122 -e name -s contact`;
-//     exec(command, (error, stdout, stderr) => {
+      if (!stdout.includes(successMessage) || !stderr.includes(warningMessage) || !stderr.includes(noteMessage)) {
+        return done(new Error('some of the Expected messages not found in stdout.'));
+      }
+      done();
+    });
+  });
 
-//         const expectedErrorMessage = 'Nonexistent flag: -w';
+  it('[BS_TC_149] Validate the behavior when an invalid field is used', (done) => {
+    const command = `sf template validate -t ${templateName}`;
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error('Command Execution Error:', error);
+        return done(error);
+      }      
+      
+      const successMessage = 'Success: SF Connection established.';
+      const warningMessage = 'Warning: Fields do not exist or cannot be accessed:\n -> account: jig';
+      const noteMessage =
+        'Note: Still we keep these populated these values, You can change them anytime from the data template!';
 
-//         if (stderr.includes(expectedErrorMessage)) {
-//         done();
-//       } else {
-//         done(new Error(`Expected error message "${expectedErrorMessage}" not found in stderr.`));
-//       }
-//     });
-//   });
-
+      if (!stdout.includes(successMessage) || !stderr.includes(warningMessage) || !stderr.includes(noteMessage)) {
+        return done(new Error('some of the Expected messages not found in stdout.'));
+      }
+      done();
+    });
+  });
 });
